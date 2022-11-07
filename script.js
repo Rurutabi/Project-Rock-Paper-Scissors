502740509ae864bc23e7ab82b882e2673a9403c1
Sira <siraphop_2538@hotmail.com>
2022-11-07 19:43:39 +1100

    Create game function

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
-const sign = ["Rock", "Paper", "Scissors"]
+const sign = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let pcScore = 0;
+let score = 0;
+let gameRound = 200;
 
-function getComputerChoice(){
-    const random = sign[Math.floor(Math.random()*sign.length)]
-    return random;
+function getComputerChoice() {
+  const random = sign[Math.floor(Math.random() * sign.length)];
+  return random;
 }
-function playRound(playerSelection, computerSelection){
-    //Player Win Condition
-   if(playerSelection == "Rock" && computerSelection == "Scissors"){
-    return "You Win! Rock beats Scissors"
-   } else if(playerSelection == "Paper" && computerSelection == "Rock"){
-    return "You Win! Paper beats Rock"
-   } else if(playerSelection == "Scissors" && computerSelection == "Paper"){
-    return "You Win! Scissors beats Paper"
+
+function getNextRandom() {
+  return getComputerChoice.shift();
+}
+
+function playRound(playerSelection, computerSelection) {
+  //Player Win Condition
+  if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return 1;
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return 1;
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return 1;
     //Computer Win condition
-    }else if(computerSelection == "Rock" && playerSelection == "Scissors"){
-    return "You lose! Computer choose Scissors"
-    } else if(computerSelection == "Paper" && playerSelection == "Rock"){
-    return "You lose! Computer choose Rock"
-    } else if(computerSelection == "Scissors" && playerSelection == "Paper"){
-     return "You lose! Computer choose  Paper"
+  } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+    return 2;
+  } else if (computerSelection == "Paper" && playerSelection == "Rock") {
+    return 2;
+  } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
+    return 2;
     //Tie
-    }else if(computerSelection == "Rock" && playerSelection == "Rock"){
-    return "Both pick Rock, Nobody win!"
-    } else if(computerSelection == "Paper" && playerSelection == "Paper"){
-    return "Both pick Paper, Nobody win!"
-    } else if(computerSelection == "Scissors" && playerSelection == "Scissors"){
-     return "Both pick Scissors, Nobody win!"
-    }
-     else
-     return "Something is wrong"
+  } else if (computerSelection == "Rock" && playerSelection == "Rock") {
+    return 0;
+  } else if (computerSelection == "Paper" && playerSelection == "Paper") {
+    return 0;
+  } else if (computerSelection == "Scissors" && playerSelection == "Scissors") {
+    return 0;
+  } else return "Something is wrong in playRound Function";
 }
 
-const playerSelection = "Rock"
-const ComputerSelection = getComputerChoice()
-console.log(playRound(playerSelection, ComputerSelection))
+function game() {
+  for (let i = 0; playerScore < 5 && pcScore < 5; i++) {
+    let computerSelection = getComputerChoice();
+    let computerSelection2 = getComputerChoice();
+    //const playerSelection = prompt("Pick Rock Paper or Scissors");
+    let winner = playRound(computerSelection2, computerSelection);
+    if (winner === 1) {
+      playerScore = playerScore + 1;
+      console.log("playerScore score is " + playerScore);
+      if (playerScore === 5) {
+        console.log("Player win");
+      }
+    } else if (winner === 2) {
+      pcScore = pcScore + 1;
+      console.log("Pc score is " + pcScore);
+      if (pcScore === 5) {
+        console.log("Pc win");
+      }
+    } else if (winner === 0) {
+      console.log("Tie");
+    } else {
+      console.log("Something went wrong");
+    }
+  }
+}
+
+console.log(game());
